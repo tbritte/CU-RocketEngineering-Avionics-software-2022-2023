29e5a07eb513d8f155df61c2f1ddc2d44a49f590
Stelath <alex.k.korte@gmail.com>
2022-09-29 20:34:32 -0400

    I did a thing...

---
+++ flight_controller/tests/test_telemetry_handler.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from flight_control import TelemetryHandler
+
+def test_telemetry_handler():
+    telemetryHandler = TelemetryHandler()
+    telemetryHandler.print_data()
+    print(f'Altitude: {telemetryHandler.calculate_altitude()}')
