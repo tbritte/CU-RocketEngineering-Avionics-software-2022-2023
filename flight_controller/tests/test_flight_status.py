5e932bdc81979ebec8ff7137564fa3baacd0eb73
Alex <alex.k.korte@gmail.com>
2022-10-06 19:15:45 -0400

    Added Flight Status Test

--- flight_controller/tests/test_flight_status.py
+++ flight_controller/tests/test_flight_status.py
@@ -1,5 +1,6 @@
-from flight_status import FlightStatus
+from flight_control import FlightStatus
 
-def test_flight_status():
-    flight_status = FlightStatus()
-    flight_status.new_telemetry({'altitude': 12, 'raw_accelerometer': 8})
+def test_current_stage():
+    flightStatus = FlightStatus()
+    assert flightStatus.current_stage() == 1
+    assert flightStatus.current_stage_name() == 'pre_flight'
