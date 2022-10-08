52d5854d5c951d29e0499a3c19d688fc9fa5a633
Alex <alex.k.korte@gmail.com>
2022-10-08 00:23:32 -0400

    90% test coverage for flight_status

--- flight_controller/tests/test_flight_status.py
+++ flight_controller/tests/test_flight_status.py
@@ -1,4 +1,3 @@
-import logging
 import random
 
 from flight_control import FlightStatus
@@ -10,13 +9,34 @@ def test_current_stage():
 
 def test_add_altitude():
     flightStatus = FlightStatus()
-    for i in range(20):
-        flightStatus.add_altitude(random.randint(0,10000))
-        
-    logging.info(flightStatus.altitude_list)
+    
+    altitudes = [random.randint(0, 100) for i in range(64)]
+    
+    for altitude in altitudes:
+        flightStatus.add_altitude(altitude)
+    
+    assert flightStatus.altitude_list == altitudes
 
 def test_check_apogee():
     flightStatus = FlightStatus()
     for i in range(64):
         flightStatus.add_altitude(i + random.randint(-10, 10))
-    flightStatus.check_apogee()
+    passed_apogee = flightStatus.check_apogee()
+    assert not passed_apogee
+    
+    for i in range(64):
+        flightStatus.add_altitude(64 - i + random.randint(-10, 10))
+    passed_apogee = flightStatus.check_apogee()
+    assert passed_apogee
+
+def test_check_apogee_extreme_noise():
+    flightStatus = FlightStatus()
+    for i in range(64):
+        flightStatus.add_altitude(i + random.randint(-50, 50))
+    passed_apogee = flightStatus.check_apogee()
+    assert not passed_apogee
+    
+    for i in range(64):
+        flightStatus.add_altitude(64 - i + random.randint(-50, 50))
+    passed_apogee = flightStatus.check_apogee()
+    assert passed_apogee
