3fbcb9985c5651273ad15d76117f29b422092406
Alex <alex.k.korte@gmail.com>
2022-10-08 00:31:04 -0400

    Added Basic Parachute Logic & Test

--- flight_controller/flight_control/parachute.py
+++ flight_controller/flight_control/parachute.py
@@ -2,7 +2,12 @@ import RPi.GPIO as GPIO
 
 class Parachute():
     def __init__(self) -> None:
-        pass
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(11, GPIO.OUT)
     
     def parachute_deployment():
-        pass
+        GPIO.output(11,GPIO.HIGH)
+        
+    def kill_parachute_signal():
+        GPIO.output(11,GPIO.LOW)
