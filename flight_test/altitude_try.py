2a360925d1611a56a6ff245875d7052b79995006
Elan456 <telan4892@gmail.com>
2022-10-27 20:30:49 -0400

    Showing stages alongside altitude using Michael's data

--- flight_test/altitude_try.py
+++ flight_test/altitude_try.py
@@ -6,9 +6,7 @@ data = pd.read_csv("altitudes.csv")
 altitudes = data["Altitude"].to_list()
 for i, a in enumerate(altitudes):
     fs.add_altitude(a)
-    if a > 64:
-        apog = fs.check_apogee()
-        print(i, a, apog)
-        if apog:
-            break
+    if i > 64:
+        fs.new_telemetry({"altitude": a})
+        print(i, a, fs.stage.name)
 
