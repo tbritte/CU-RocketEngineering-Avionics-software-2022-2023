16bf63425d2b7a87971ac4f415c412847fee0b32
Alex <alex.k.korte@gmail.com>
2023-01-22 18:00:31 -0500

    Started Created Testing Program

---
+++ AntennaTester/AntennaTester/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.IO.Ports;
+
+Console.WriteLine("Hello, World!");
+
+SerialPort serialPort = new SerialPort(portName: "COM1", baudRate: 57600, parity: Parity.None, dataBits: 8, stopBits: StopBits.One);
+
