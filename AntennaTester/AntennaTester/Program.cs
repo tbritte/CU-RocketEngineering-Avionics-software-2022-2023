ca4bb79f42ad1a65bb16680a7c482093f15caeb3
Alex <alex.k.korte@gmail.com>
2023-01-23 09:38:41 -0500

    Added a bit more to the antenna tester

--- AntennaTester/AntennaTester/Program.cs
+++ AntennaTester/AntennaTester/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.IO.Ports;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting Antenna Tester!");
+Console.WriteLine("Which COM Port Would You Like to Use? ");
+string comPort = Console.ReadLine();
+
+while(comPort == null)
+{
+    Console.WriteLine("Please enter a comm port:");
+}
 
 SerialPort serialPort = new SerialPort(portName: "COM1", baudRate: 57600, parity: Parity.None, dataBits: 8, stopBits: StopBits.One);
 
