69df6e3d8ec836e5d6502ebee2ec921bf183e4ba
Elan456 <106495544+Elan456@users.noreply.github.com>
2023-01-31 19:36:11 -0500

    Added try except around update_lights

--- flight_controller/flight_control/LED_controller.py
+++ flight_controller/flight_control/LED_controller.py
@@ -17,19 +17,23 @@ class LEDController:
             self.sense.set_pixel(i, row, color)
 
     def update_lights(self):
-        self.tick += 1
-        self.sense.clear()
-        # Getting the feet in altitude
-        alt_ft = stat.median(self.flight_status.altitude_list) * 3.28
-        stage_num = self.flight_status.stage.value
-        self.fill_row(0, stage_num - 1, [0, 0, 255])
-        self.fill_row(1, min(math.floor(alt_ft * 7 / 10000), 7), [255, 0, 0])
-        
-        
-        # Blinking green status light
-        if self.tick < 10:
-            self.sense.set_pixel(7, 7, [0, 255, 0])
-        elif self.tick > 20:
-            self.tick = 0
+        try:
+            self.tick += 1
+            self.sense.clear()
+            # Getting the feet in altitude
+            alt_ft = stat.median(self.flight_status.altitude_list) * 3.28
+            stage_num = self.flight_status.stage.value
+            self.fill_row(0, stage_num - 1, [0, 0, 255])
+            self.fill_row(1, min(math.floor(alt_ft * 7 / 10000), 7), [255, 0, 0])
+
+
+            # Blinking green status light
+            if self.tick < 10:
+                self.sense.set_pixel(7, 7, [0, 255, 0])
+            elif self.tick > 20:
+                self.tick = 0
+        except:
+            print("Light update fail")
+            
 
 
