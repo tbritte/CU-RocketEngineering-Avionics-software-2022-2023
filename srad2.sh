64858303c5a8c46bc1622f947ccd6bdb52086e09
EthanDesktop <telan4892@gmail.com>
2023-06-18 01:56:46 -0400

    Removed the .py from simple_srad2.py

--- srad2.sh
+++ srad2.sh
@@ -1,3 +1,3 @@
 
 cd /home/curocket/CU-RocketEngineering-Avionics-software-2022-2023/flight_controller
-sudo -u curocket python3 -m flight_control.simple_srad2.py
+sudo -u curocket python3 -m flight_control.simple_srad2
